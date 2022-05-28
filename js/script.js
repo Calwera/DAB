949f70ad8eeb1d8db70a1d95b1af347576b084b1
Calwera <calwera@gmail.com>
2022-05-28 11:16:32 +0200

    reanimating the project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const dodaj = document.querySelector(".dodaj");
+const close_popup = document.querySelector(".popup__add-exit");
+const popup = document.querySelector(".popup");
+
+dodaj.addEventListener("click", function () {
+  popup.style.display = "block";
+});
+
+// dodaj.addEventListener("click", function () {
+//   popup.style.display = "none";
+// });
