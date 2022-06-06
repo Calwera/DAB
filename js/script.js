b5070428d151289d58bf671a24e0b729371ef545
Calwera <calwera@gmail.com>
2022-06-06 21:56:33 +0200

    redefind modal mechanic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 "use strict";
-const nav = document.querySelectorAll(".main-page__nav-list-item");
-const close_popup = document.querySelector(".popup__add-exit");
+const nav = document.querySelectorAll("[data-modal-target]");
+const close_popup = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
+const overlay = document.querySelector(".popup__overlay");
 
-nav.forEach((button) => {
-  button.addEventListener("click", function () {
-    popup.style.display = "block";
+nav.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    const modal = document.querySelector(nav.dataset.modalTarget);
+    openModal(modal);
   });
 });
 
 close_popup.addEventListener("click", function () {
-  popup.style.display = "none";
+  closeModal(popup);
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".popup.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
 });
