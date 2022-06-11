2bc2a266f803dfe3197dd1175d77c979f2b0e13c
Calwera <calwera@gmail.com>
2022-06-11 12:00:19 +0200

    popup show added

--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
 "use strict";
 const nav = document.querySelectorAll("[data-modal-target]");
-const close_popup = document.querySelector(".popup__close-button");
+const close_popup = document.querySelectorAll(".popup__close-button");
 const popup = document.querySelector(".popup");
 const overlay = document.querySelector(".popup__overlay");
+const page = document.querySelector(".main-page");
+let modal;
 
 nav.forEach((nav) => {
   nav.addEventListener("click", () => {
-    const modal = document.querySelector(nav.dataset.modalTarget);
+    modal = document.querySelector(nav.dataset.modalTarget);
     openModal(modal);
   });
 });
 
-close_popup.addEventListener("click", function () {
-  closeModal(popup);
+close_popup.forEach((close) => {
+  close.addEventListener("click", function () {
+    closeModal(modal);
+  });
 });
 
 function openModal(modal) {
-  if (modal == null) return;
+  // if (modal == null) return;
   modal.classList.add("active");
   overlay.classList.add("active");
+  page.classList.add("disable");
 }
 
 function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
   overlay.classList.remove("active");
+  page.classList.remove("disable");
 }
 
 overlay.addEventListener("click", () => {
