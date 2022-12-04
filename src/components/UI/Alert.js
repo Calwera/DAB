d2e682193ee9a1c39c9d017b1ec6acb6b53e5f26
Calwera <calwera@gmail.com>
2022-12-04 11:09:48 +0100

    styled Alert component

--- src/components/UI/Alert.js
+++ src/components/UI/Alert.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { Fragment, useEffect } from "react";
 
 const Alert = (props) => {
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        props.onDeleteHandler(null);
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
+
   return (
-    <div className="alert">
-      <h2 className="alert__header">{props.message}</h2>
-      <button onClick={() => props.onDeleteHandler(props.id)}>Tak</button>
-      <button onClick={() => props.onDeleteHandler(null)}>Nie</button>
-    </div>
+    <Fragment>
+      <div className="alert">
+        <h2 className="alert__header">{props.message}</h2>
+        <div className="alert__container">
+          <button
+            className="alert__confirm"
+            onClick={() => props.onDeleteHandler(props.id)}
+          >
+            Tak
+          </button>
+          <button
+            className="alert__cancel"
+            onClick={() => props.onDeleteHandler(null)}
+          >
+            Nie
+          </button>
+        </div>
+      </div>
+      <div
+        onClick={() => props.onDeleteHandler(null)}
+        className="popup__overlay active"
+      ></div>
+    </Fragment>
   );
 };
 
