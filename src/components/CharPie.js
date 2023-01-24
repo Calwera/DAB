0869dfdedacdb2466eaaa3c21fe9e9fa9cad6a91
Krzysztof Wróbel <calwera@gmail.com>
2023-01-24 21:09:38 +0100

    Pie Chart implemented

--- src/components/CharPie.js
+++ src/components/CharPie.js
@@ -2,18 +2,68 @@ import React, { useState } from "react";
 import { useCost } from "../contexts/CostContext";
 import { Pie } from "react-chartjs-2";
 
-const CharPie = (props) => {
+const CharPie = () => {
   const ctx = useCost();
+  console.log();
+  let costSummary = [];
+  costSummary.push({
+    category: "Jedzenie",
+    price: ctx.cost
+      .filter((obj) => {
+        if (!obj.category.localeCompare("Jedzenie")) return obj.price;
+      })
+      .reduce((total, obj) => {
+        return +obj.price + total;
+      }, 0),
+  });
+  costSummary.push({
+    category: "Rachunki",
+    price: ctx.cost
+      .filter((obj) => {
+        if (!obj.category.localeCompare("Rachunki")) return obj.price;
+      })
+      .reduce((total, obj) => {
+        return +obj.price + total;
+      }, 0),
+  });
+  costSummary.push({
+    category: "Zakupy",
+    price: ctx.cost
+      .filter((obj) => {
+        if (!obj.category.localeCompare("Zakupy")) return obj.price;
+      })
+      .reduce((total, obj) => {
+        return +obj.price + total;
+      }, 0),
+  });
+  costSummary.push({
+    category: "Raty",
+    price: ctx.cost
+      .filter((obj) => {
+        if (!obj.category.localeCompare("Raty")) return obj.price;
+      })
+      .reduce((total, obj) => {
+        return +obj.price + total;
+      }, 0),
+  });
+  costSummary.push({
+    category: "Inne",
+    price: ctx.cost
+      .filter((obj) => {
+        if (!obj.category.localeCompare("Inne")) return obj.price;
+      })
+      .reduce((total, obj) => {
+        return +obj.price + total;
+      }, 0),
+  });
 
-  // console.log(ctx.cost);
-  // const costSum = ctx.cost.
-
+  console.log(costSummary);
   const [chartData, setChartData] = useState({
-    labels: ctx.cost.map((data) => data.category),
+    labels: costSummary.map((data) => data.category),
     datasets: [
       {
         label: "Kategorie ",
-        data: ctx.cost.map((data) => data.price),
+        data: costSummary.map((data) => data.price),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#ecf0f1",
